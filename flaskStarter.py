c12355f1434a2dd15f435e67f8493841b53937c0
Óðinn Dagur Bjarnason <36035737+odinndagur@users.noreply.github.com>
2021-10-25 14:00:14 +0000

    l

--- flaskStarter.py
+++ flaskStarter.py
@@ -11,6 +11,18 @@ def response_page():
     name = request.form['name']    
     return render_template('response_page.html', name=name)
 
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_filel():
+   if request.method == 'POST':
+        f = request.files['file']
+        print(f)
+        filename = f.filename
+        # f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],'input.pdf'))
+        file = os.path.join(app.config['UPLOAD_FOLDER'],'input.pdf')
+        files = {'file':file}
+        #resp = requests.get('http://127.0.0.1:8001/uploader')
+        return filename
 if __name__ == "__main__":
         port = int(os.environ.get("PORT", 5000))
         app.run(host='0.0.0.0', port=port)
